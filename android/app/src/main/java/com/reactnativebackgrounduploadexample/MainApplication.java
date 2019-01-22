d5c99c93f156f6d39f393e65f50abeccb6668aa2
Stephen Potter <steve.potter@vydia.com>
2019-01-22 11:12:57 -0500

    update packages to fix babel error

--- android/app/src/main/java/com/reactnativebackgrounduploadexample/MainApplication.java
+++ android/app/src/main/java/com/reactnativebackgrounduploadexample/MainApplication.java
@@ -29,6 +29,11 @@ public class MainApplication extends Application implements ReactApplication {
             new UploaderReactPackage()
       );
     }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
   };
 
   @Override
